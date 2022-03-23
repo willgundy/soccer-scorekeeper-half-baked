58a8fc08c62190092aedbda21b5b62829e677032
Will Gundy <willgunderson503@gmail.com>
2022-03-23 15:23:16 -0700

    Finished functions in fetch utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -19,12 +19,18 @@ The games are stored in the database using this data model:
 
 export async function createGame(game){
     // create a single new game in the games table using the above object
+    const response = await client
+        .from('games')
+        .insert(game);
     
     return checkError(response);
 }
 
 export async function getGames() {
     // select all games from the games table
+    const response = await client
+        .from('games')
+        .select('*');
 
     return checkError(response);    
 }
